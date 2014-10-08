d46f8d80d2247c33ea1994ff02c041db84b73195
Ethan Vizitei <ethan.vizitei@gmail.com>
2014-10-07 18:09:25 -0600

    explore various speclj functions:

--- spec/change_counter/core_spec.clj
+++ spec/change_counter/core_spec.clj
@@ -8,4 +8,30 @@
   (it "tests if true-or-false returns true"
     (should (true-or-false))))
 
+(defn equals-5? [n]
+  (= 5 n))
+
+(describe "Understanding Speclj Components"
+  (before-all
+    (println "Starting testing"))
+
+  (after-all
+    (println "finished testing"))
+
+  (before
+    (println "Before each test"))
+
+  (after
+    (println "After each test"))
+
+  (it "confirms 4 plus 1 equals 5"
+    (println "Running first test")
+    (should (equals-5? (+ 4 1))))
+
+  (it "confirms 4 plus 2 isn't 5"
+    (should-not (equals-5? (+ 4 2))))
+
+  (it "evaluates statements inline too"
+    (should= 6 (+ 4 2)))
+)
 (run-specs)
